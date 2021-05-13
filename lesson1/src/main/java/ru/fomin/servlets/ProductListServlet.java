73ab02f0472be3523148e5be058c3c9c5a1e6b42
Dmitriy Fomin <79067773397@yandex.ru>
2021-05-13 21:40:54 +0300

    add new project

--- lesson1/src/main/java/ru/fomin/servlets/ProductListServlet.java
+++ lesson1/src/main/java/ru/fomin/servlets/ProductListServlet.java
@@ -1,7 +1,6 @@
 package ru.fomin.servlets;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ConfigurableApplicationContext;
+
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import ru.fomin.config.AppConfig;
 import ru.fomin.service.ProductService;
