e4f79f0ef6183b4c560c363ba25f1adcb5ace81c
Dmitriy Fomin <79067773397@yandex.ru>
2021-06-16 17:52:03 +0300

    added

--- free_progect/src/main/java/ru/fomin/free_progect/services/impl/CartServiceImpl.java
+++ free_progect/src/main/java/ru/fomin/free_progect/services/impl/CartServiceImpl.java
@@ -1,2 +1,34 @@
-package ru.fomin.free_progect.services.impl;public class CartServiceImpl {
+package ru.fomin.free_progect.services.impl;
+
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.fomin.free_progect.beans.Cart;
+import ru.fomin.free_progect.domains.OrderItem;
+import ru.fomin.free_progect.services.CartService;
+import ru.fomin.free_progect.services.ProductPriceService;
+
+import javax.annotation.Resource;
+
+@Service
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class CartServiceImpl implements CartService {
+
+    @Resource
+    ProductPriceService productPriceService;
+
+    Cart cart;
+
+    @Autowired
+    public void setCart(Cart cart) {
+        this.cart = cart;
+    }
+
+    @Override
+    public void addProduct(Long productId) {
+        OrderItem addingOrderItem = productPriceService.getOrderItem(productId);
+        cart.addProduct(addingOrderItem);
+    }
+
 }
--- free_progect/src/main/java/ru/fomin/free_progect/services/impl/ProductPriceServiceImpl.java
+++ free_progect/src/main/java/ru/fomin/free_progect/services/impl/ProductPriceServiceImpl.java
@@ -1,2 +1,33 @@
-package ru.fomin.free_progect.services.impl;public class ProductPriceServiceImpl {
+package ru.fomin.free_progect.services.impl;
+
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import ru.fomin.free_progect.domains.OrderItem;
+import ru.fomin.free_progect.entities.ProductPriceEn;
+import ru.fomin.free_progect.mappers.OrderItemMapper;
+import ru.fomin.free_progect.repositories.ProductPriceRepository;
+import ru.fomin.free_progect.services.ProductPriceService;
+
+import javax.annotation.Resource;
+
+@Service
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ProductPriceServiceImpl implements ProductPriceService {
+
+    @Resource
+    ProductPriceRepository productPriceRepository;
+
+    @Resource
+    OrderItemMapper orderItemMapper;
+
+    @Override
+    public OrderItem getOrderItem(Long productId) {
+        ProductPriceEn productPriceEn = productPriceRepository.findByProduct_Id(productId)
+                .orElseThrow(() -> new RuntimeException("product was not found"));
+        return orderItemMapper.convertToOrderItem(productPriceEn);
+    }
+
+
 }
