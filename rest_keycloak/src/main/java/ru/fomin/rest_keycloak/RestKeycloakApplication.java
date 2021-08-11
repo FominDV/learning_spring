88f33e310ff1aedef0777e1c23dceb64071ba4ae
Dmitriy Fomin <79067773397@yandex.ru>
2021-08-11 19:12:59 +0300

    end coping

---
+++ rest_keycloak/src/main/java/ru/fomin/rest_keycloak/RestKeycloakApplication.java
@@ -0,0 +1,13 @@
+package ru.fomin.rest_keycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestKeycloakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestKeycloakApplication.class, args);
+	}
+
+}
---
+++ rest_keycloak/src/test/java/ru/fomin/rest_keycloak/RestKeycloakApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.fomin.rest_keycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestKeycloakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
