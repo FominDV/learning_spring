5861189bb2366d4e2c1c9b4068698e25dd96c916
Dmitriy Fomin <79067773397@yandex.ru>
2021-09-05 04:04:43 +0300

    config-service was finished

--- spring-cloud/config/src/main/java/ru/fomin/ConfigServerApp.java
+++ spring-cloud/config/src/main/java/ru/fomin/ConfigServerApp.java
@@ -3,10 +3,11 @@ package ru.fomin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer
-//@EnableEurekaClient
+@EnableEurekaClient
 public class ConfigServerApp {
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApp.class, args);
