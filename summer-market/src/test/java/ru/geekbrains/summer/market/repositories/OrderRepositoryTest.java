eaa91ce6c7083df67bab55b4345377fd9afdeb3e
Dmitriy Fomin <79067773397@yandex.ru>
2021-09-14 22:37:12 +0300

    test OrderRepositoryTest#findAllByUsername was finished

--- summer-market/src/test/java/ru/geekbrains/summer/market/repositories/OrderRepositoryTest.java
+++ summer-market/src/test/java/ru/geekbrains/summer/market/repositories/OrderRepositoryTest.java
@@ -1,6 +1,8 @@
 package ru.geekbrains.summer.market.repositories;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -9,7 +11,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import ru.geekbrains.summer.market.model.Order;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 @ActiveProfiles("Test")
 @DataJpaTest
 class OrderRepositoryTest {
@@ -21,8 +26,19 @@ class OrderRepositoryTest {
     private TestEntityManager entityManager;
 
     @Test
-    public void save(){
+    public void save() {
 
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "user, 1",
+            "admin, 0"
+    })
+    public void findAllByUsername(String username, int expectedSize) {
+        List<Order> orderList = orderRepository.findAllByUsername(username);
+        assertNotNull(orderList);
+        assertEquals(expectedSize, orderList.size());
     }
 
 }
