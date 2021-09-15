1d9dd6195c496a662103b55d5978e1b71a9637c4
Dmitriy Fomin <79067773397@yandex.ru>
2021-09-15 06:22:08 +0300

    OrderRepositoryTest#save was finished

--- summer-market/src/test/java/ru/geekbrains/summer/market/repositories/OrderRepositoryTest.java
+++ summer-market/src/test/java/ru/geekbrains/summer/market/repositories/OrderRepositoryTest.java
@@ -4,16 +4,19 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import ru.geekbrains.summer.market.model.Order;
+import ru.geekbrains.summer.market.model.OrderItem;
+import ru.geekbrains.summer.market.model.Product;
+import ru.geekbrains.summer.market.model.User;
 
+import java.math.BigDecimal;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @ActiveProfiles("Test")
 @DataJpaTest
@@ -27,7 +30,25 @@ class OrderRepositoryTest {
 
     @Test
     public void save() {
+        String address = "Address exp";
+        String phone = "89098880921";
+        Order order = new Order();
+        order.setAddress(address);
+        order.setPhone(phone);
+        List<OrderItem> orderItemList = List.of(
+                getOrderItem(entityManager.find(Product.class, 6L), 5, order),
+                getOrderItem(entityManager.find(Product.class, 3L), 2, order)
+        );
+        BigDecimal orderPrice = orderItemList.stream()
+                .map(OrderItem::getPrice)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+        order.setItems(orderItemList);
+        order.setUser(entityManager.find(User.class, 2L));
+        order.setPrice(orderPrice);
 
+        orderRepository.save(order);
+
+        assertEquals(2, orderRepository.findAll().size());
     }
 
     @ParameterizedTest
@@ -39,6 +60,16 @@ class OrderRepositoryTest {
         List<Order> orderList = orderRepository.findAllByUsername(username);
         assertNotNull(orderList);
         assertEquals(expectedSize, orderList.size());
+    }
+
+    private OrderItem getOrderItem(Product product, int quantity, Order order) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setProduct(product);
+        orderItem.setQuantity(quantity);
+        orderItem.setPricePerProduct(product.getPrice());
+        orderItem.setPrice(product.getPrice().multiply(BigDecimal.valueOf(quantity)));
+        orderItem.setOrder(order);
+        return orderItem;
     }
 
 }
