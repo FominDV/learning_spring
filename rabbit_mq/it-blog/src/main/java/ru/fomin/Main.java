01f9b4589fece26fe19cd1178e89a752dd77a299
Dmitriy Fomin <79067773397@yandex.ru>
2021-09-17 22:53:45 +0300

    producer was created

---
+++ rabbit_mq/it-blog/src/main/java/ru/fomin/Main.java
@@ -0,0 +1,9 @@
+package ru.fomin;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new ArticleProducer().execute();
+    }
+
+}
--- rabbit_mq/subscriber/src/main/java/ru/fomin/ArticleConsumer.java
+++ rabbit_mq/subscriber/src/main/java/ru/fomin/ArticleConsumer.java
@@ -2,7 +2,7 @@ package ru.fomin;
 
 public class ArticleConsumer {
 
-    public static void main(String[] args) {
+    public void execute() {
 
     }
 
