121c606d030f7ff043bfd9678f6e25026e337453
Dmitriy Fomin <79067773397@yandex.ru>
2021-09-17 20:08:44 +0300

    docker-compose rabbitMQ was created

---
+++ rabbit_mq/it-blog/src/main/java/ru/fomin/ArticleProducer.java
@@ -0,0 +1,9 @@
+package ru.fomin;
+
+public class ArticleProducer {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ rabbit_mq/subscriber/src/main/java/ru/fomin/ArticleConsumer.java
@@ -0,0 +1,9 @@
+package ru.fomin;
+
+public class ArticleConsumer {
+
+    public static void main(String[] args) {
+
+    }
+
+}
