43ae26b2721722888665bc750b648f60cf8522b5
Dmitriy Fomin <79067773397@yandex.ru>
2021-09-28 19:52:28 +0300

    start learning PayPal

--- summer-market/src/main/java/ru/geekbrains/summer/market/SummerMarketApplication.java
+++ summer-market/src/main/java/ru/geekbrains/summer/market/SummerMarketApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
+// Параметр VM Options для выбора профиля: -Dspring.profiles.active=dev
+
 @SpringBootApplication
 @PropertySource("classpath:secret.properties")
 public class SummerMarketApplication {
 	// Домашнее задание:
-	// 1. Исследовательская задача: прикрутить картинки к товарам
+	// 1. Попробуйте допилить PayPal
 
 	// План по магазину:
 
@@ -16,6 +18,7 @@ public class SummerMarketApplication {
 	// - Swagger
 	// - Платежная система (PayPal)
 	// - Картинки
+	// - Разобраться с запуском memurai
 
 	// Занятие 12:
 	// - KeyCloak
