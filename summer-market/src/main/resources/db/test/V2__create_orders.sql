cf9cb22967a25d489c73583fc136138d5ac1fd3f
Dmitriy Fomin <79067773397@yandex.ru>
2021-09-29 20:06:13 +0300

    edit front

--- summer-market/src/main/resources/db/test/V2__create_orders.sql
+++
@@ -1,8 +0,0 @@
-insert into orders
-    (price, user_id, address, phone)
-values (246, 1, 'address', '89999803926');
-
-insert into order_items
-    (price, price_per_product, product_id, order_id, quantity)
-values (190, 95, 1, 1, 2),
-       (56, 28, 2, 1, 2);
